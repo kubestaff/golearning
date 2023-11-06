b717444f94cf2d61d6ec0a1c214d6d1ac09607a8
Moses <mac@macs-mbp.home>
2023-11-06 18:39:34 +0000

    added me page

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/kubestaff/web-helper/server"
 	"net/http"
 	"time"
+
+	"github.com/kubestaff/web-helper/server"
 )
 
 func main() {
@@ -20,6 +21,7 @@ func main() {
 	s.Handle("/", HandleIndex)
 	s.Handle("/status", HandleStatus)
 	s.Handle("/months", HandleMonths)
+	s.Handle("/me", HandleMe)
 	s.HandleJSON("/colors", HandleJsonOutput)
 	s.HandleJSON("/add-color", HandleJsonInputFromParams)
 	s.HandleJSON("/add-color-json", HandleJsonInputFromBody)
@@ -157,6 +159,9 @@ func HandleJsonOutput(input server.Input) (o server.Output) {
 
 func HandleStatus(inputs server.Input) (filename string, placeholders map[string]string) {
 	return "html/status.html", nil
+}
+func HandleMe(inputs server.Input) (filename string, placeholders map[string]string) {
+	return "html/me.html", nil
 }
 
 func HandleIndex(inputs server.Input) (filename string, placeholders map[string]string) {
