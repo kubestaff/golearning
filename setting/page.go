51a39ee0e21b39d0ca6b08a8db963c81fe7dea11
judith <judithuk11@yahoo.co.uk>
2023-11-22 17:20:53 +0000

    CRUD

--- setting/page.go
+++ setting/page.go
@@ -86,3 +86,34 @@ func (h Handler) HandleFormSetting(inputs server.Input) (filename string, placeh
 
 	return "html/setting.html", output
 }
+func (h Handler) HandleDeleteUserSetting(inputs server.Input) (filename string, placeholders map[string]string) {
+	userIdStr := inputs.Values.Get("id")
+
+	userIdInt := 0
+	var err error
+	if userIdStr != "" {
+		userIdInt, err = strconv.Atoi(userIdStr)
+		if err != nil {
+			return helper.HandleErrorText("Invalid user id")
+		}
+	}
+
+	userProvider := Provider{
+		DbConnection: h.DbConnection,
+	}
+	usr, isFound, err := userProvider.GetSettingByUserId(userIdInt)
+	if err != nil {
+		return helper.HandleErr(err)
+	}
+
+	if !isFound {
+		return helper.HandleErrorText("User id is not found")
+	}
+
+	err = userProvider.DeleteSetting(&usr)
+	if err != nil {
+		return helper.HandleErr(err)
+	}
+
+	return "html/success.html", map[string]string{"%success%": "Successfully deleted user"}
+}
