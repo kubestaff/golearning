2a0bda92829c3bc7a7648279853f3efc4004c176
Oreva Eniworo <eniworoeva@yahoo.com>
2023-11-23 18:54:31 +0100

    changes

---
+++ user/page.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"strconv"
+
+	"github.com/kubestaff/web-helper/server"
+)
+
+
+func HandleMe(inputs server.Input) (filename string, placeholders map[string]string) {
+	userIdStr := inputs.Values.Get("id")
+	userIdInt, err := strconv.Atoi(userIdStr)
+	if err != nil {
+		//todo
+		return "", nil
+	}
+
+	userProvider := Provider{}
+	user, isFound := userProvider.GetUserById(userIdInt)
+	if !isFound {
+		//todo
+		return "", nil
+	}
+
+
+
+	variables := map[string]string{
+		"%name%": user.Name,
+	}
+	return "html/me.html", variables
+}
