e46c9348c35520ed7f6a2373defc027e6dbdad0d
Oreva Eniworo <eniworoeva@yahoo.com>
2023-12-01 10:25:53 +0100

    more changes

--- user/page.go
+++ user/page.go
@@ -11,22 +11,20 @@ func HandleMe(inputs server.Input) (filename string, placeholders map[string]str
 	userIdStr := inputs.Values.Get("id")
 	userIdInt, err := strconv.Atoi(userIdStr)
 	if err != nil {
-		//todo
-		return "", nil
+		return helpers.HandleErrorText("Invalid user id")
 	}
 
 	userProvider := Provider{}
 	user, isFound := userProvider.GetUserById(userIdInt)
 	if !isFound {
-		//todo
-		return "", nil
+		return helpers.HandleErrorText("User not found")
 	}
 
 	characteristicsStr := helpers.WrapStringsToTags(user.Characteristics, "li")
 	likesStr := helpers.WrapStringsToTags(user.Likes, "li")
 	dislikesStr := helpers.WrapStringsToTags(user.Dislikes, "li")
 
-	variables := map[string]string{
+	output := map[string]string{
 		"%name%":            user.Name,
 		"%job-title%":       user.JobTitle,
 		"%age%":             strconv.Itoa(user.Age),
@@ -34,6 +32,11 @@ func HandleMe(inputs server.Input) (filename string, placeholders map[string]str
 		"%characteristics%": characteristicsStr,
 		"%likes%":           likesStr,
 		"%dislikes%":        dislikesStr,
+		"%about%":           user.About,
+		"%backgroundColor%": user.BackgroundColor,
+		"%nameFontColor%:":  user.NameFontColor,
+		"%jobFontColor%":    user.JobFontColor,
+		"%ageFontColor%":    user.AgeFontColor,
 	}
-	return "html/me.html", variables
+	return "html/me.html", output
 }
