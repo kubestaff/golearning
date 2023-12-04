710bd22ce83e8c165ca09cd99b3728050cd198ba
Andrey Pozolotin <posandrey@gmail.com>
2023-12-04 22:14:31 +0100

    Handling user create and update

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -4,9 +4,11 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useParams } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 
 import React, { useState, useEffect } from 'react';
 const backendUrl = "http://localhost:34567/users?id=";
+const changeUserUrl = "http://127.0.0.1:34567/user-change?id=";
 
 export default function Users() {
   return (
@@ -50,11 +52,32 @@ export function Change() {
   }, [])
 
   const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+    e.preventDefault()
+    const data = {
+      Age: age,
+      Name: firstName,
+      JobTitle: jobTitle,
+    }
+  
+    fetch(changeUserUrl + userId, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(response => {
+      console.log(response.json())
+    })
   };
 
   return (
     <>
+     <Alert variant={"danger"} dismissible>
+         Some error happened
+     </Alert>
+     <Alert variant={"success"} dismissible>
+         User successfully saved
+     </Alert>
     <h2>Change User </h2>
     <Form className="bg-gray p-3" onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="name">
@@ -75,22 +98,42 @@ export function Change() {
 }
 
 export function Create() {
-  const handleClick = () => {
-    alert("Button is clicked")
+  const [firstName, setFirstName] = useState('');
+  const [age, setAge] = useState(0);
+  const [jobTitle, setJobTitle] = useState('');
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    const data = {
+      Age: age,
+      Name: firstName,
+      JobTitle: jobTitle,
+    }
+  
+    fetch(changeUserUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(response => {
+      console.log(response.json())
+    })
   };
 
   return (
     <>
+    
     <h2>Create user</h2>
-    <Form className="bg-gray p-3" onSubmit={handleClick}>
+    <Form className="bg-gray p-3" onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="name">
-        <Form.Control type="text" placeholder="Name" required />
+        <Form.Control type="text" placeholder="Name" required onChange={e => setFirstName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="age">
-        <Form.Control type="number" placeholder="Age" min={0} max={100} required/>
+        <Form.Control type="number" placeholder="Age" min={0} max={100} required onChange={e => setAge(Number(e.target.value))}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="job">
-        <Form.Control type="text" placeholder="Job Title" required />
+        <Form.Control type="text" placeholder="Job Title" required onChange={e => setJobTitle(e.target.value)} />
       </Form.Group>
       <Button variant="primary" type="submit">
         Save
