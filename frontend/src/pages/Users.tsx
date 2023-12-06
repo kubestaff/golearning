2510ca099a8b6b635596286ad5b9a7ce08e2c7ee
“Funto <info@funtoawoyelu.com>
2023-12-06 14:18:07 +0000

    changes to user page

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -7,6 +7,7 @@ import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
 
 import React, { useState, useEffect } from 'react';
+import { error } from "console";
 const backendUrl = "http://localhost:34567/users?id=";
 const changeUserUrl = "http://127.0.0.1:34567/user-change?id=";
 
@@ -42,6 +43,7 @@ export function Change() {
   const [jobCol, setJobcol] = useState('');
   const [ageCol, setAgecol] = useState('');
   const [about, setAbout] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const fetchUserData = function () {
     fetch(backendUrl + userId)
@@ -75,8 +77,17 @@ export function Change() {
       Age: age,
       Name: firstName,
       JobTitle: jobTitle,
+      Characteristics: characteristics,
+      Likes: likes,
+      Dislikes: dislikes,
+      Image: image,
+      Backgroundcol: backgroundCol,
+      Namecol: nameCol,
+      Jobcol: jobCol,
+      Agecol: ageCol,
+      About: about,
     }
-  
+
     fetch(changeUserUrl + userId, {
       method: "POST",
       headers: {
@@ -87,38 +98,88 @@ export function Change() {
       console.log(response.json())
     })
   };
-
   return (
     <>
-     <Alert variant={"danger"} dismissible>
-         Some error happened
-     </Alert>
-     <Alert variant={"success"} dismissible>
-         User successfully saved
-     </Alert>
-    <h2>Change User </h2>
-    <Form className="bg-gray p-3" onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Control type="text" placeholder="Name" value={firstName} required onChange={e => setFirstName(e.target.value)} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="age">
-        <Form.Control type="number" placeholder="Age" value={age} min={0} max={100} required onChange={e => setAge(Number(e.target.value))}/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="job">
-        <Form.Control type="text" placeholder="Job Title" required value={jobTitle} onChange={e => setJobTitle(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Save
-      </Button>
-    </Form>
+      <Alert variant={"danger"} onClose={() => setIsValid(false)} dismissible>
+        Some error happened
+      </Alert>
+      <Alert variant={"success"} dismissible>
+        User successfully saved
+      </Alert>
+      <h2>Change User </h2>
+      <Form className="bg-gray p-3" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" value={firstName} required onChange={e => setFirstName(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="age">
+          <Form.Label>Age</Form.Label>
+          <Form.Control type="number" value={age} min={0} max={100} required onChange={e => setAge(Number(e.target.value))} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="job">
+          <Form.Label>Job Title</Form.Label>
+          <Form.Control type="text" required value={jobTitle} onChange={e => setJobTitle(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="characteristics">
+          <Form.Label >Characteristics</Form.Label>
+          <Form.Control size="sm" type="text" as="textarea" value={characteristics} onChange={e => setCharacteristics(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="likes">
+          <Form.Label >Likes</Form.Label>
+          <Form.Control size="sm" type="text" as="textarea" value={likes} onChange={e => setLikes(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="dislikes">
+          <Form.Label >Dislikes</Form.Label>
+          <Form.Control size="sm" type="text" as="textarea" value={dislikes} onChange={e => setDislikes(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="img">
+          <Form.Label>Image</Form.Label>
+          <Form.Control type="file" value={image} onChange={e => setImage(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="background colour">
+          <Form.Label htmlFor="exampleColorInput">Choose your Background color</Form.Label>
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#FFFFFF" placeholder="Choose your color" value={backgroundCol} onChange={e => setBackground(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="name font colour">
+          <Form.Label htmlFor="exampleColorInput">Choose your Name font color</Form.Label>
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" value={nameCol} onChange={e => setNamecol(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="job font colour">
+          <Form.Label htmlFor="exampleColorInput">Choose your Job font color</Form.Label>
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" value={jobCol} onChange={e => setJobcol(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="age font colour">
+          <Form.Label htmlFor="exampleColorInput">Choose your Age font color</Form.Label>
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" value={ageCol} onChange={e => setAgecol(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="about">
+          <Form.Label >Tell us about yourself</Form.Label>
+          <Form.Control size="lg" type="text" as="textarea" onChange={e => setAbout(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Save
+        </Button>
+      </Form>
     </>
   );
-}
+};
+
+
+
 
 export function Create() {
   const [firstName, setFirstName] = useState('');
   const [age, setAge] = useState(0);
   const [jobTitle, setJobTitle] = useState('');
+  const [characteristics, setCharacteristics] = useState('');
+  const [likes, setLikes] = useState('');
+  const [dislikes, setDislikes] = useState('');
+  const [image, setImage] = useState('');
+  const [backgroundCol, setBackground] = useState('');
+  const [nameCol, setNamecol] = useState('');
+  const [jobCol, setJobcol] = useState('');
+  const [ageCol, setAgecol] = useState('');
+  const [about, setAbout] = useState('');
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault()
@@ -126,8 +187,17 @@ export function Create() {
       Age: age,
       Name: firstName,
       JobTitle: jobTitle,
+      Characteristics: characteristics,
+      Likes: likes,
+      Dislikes: dislikes,
+      Image: image,
+      Backgroundcol: backgroundCol,
+      Namecol: nameCol,
+      Jobcol: jobCol,
+      Agecol: ageCol,
+      About: about,
     }
-  
+
     fetch(changeUserUrl, {
       method: "POST",
       headers: {
@@ -141,7 +211,7 @@ export function Create() {
 
   return (
     <>
-    
+
       <h2>Create User</h2>
       <Form className="bg-gray p-3" onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="name">
@@ -154,43 +224,43 @@ export function Create() {
         </Form.Group>
         <Form.Group className="mb-3" controlId="job">
           <Form.Label >Job Title</Form.Label>
-          <Form.Control type="text" required />
+          <Form.Control type="text" required onChange={e => setJobTitle(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="characteristics">
           <Form.Label >Characteristics</Form.Label>
-          <Form.Control size="sm" type="text" as="textarea" />
+          <Form.Control size="sm" type="text" as="textarea" onChange={e => setCharacteristics(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="likes">
           <Form.Label >Likes</Form.Label>
-          <Form.Control size="sm" type="text" as="textarea" />
+          <Form.Control size="sm" type="text" as="textarea" onChange={e => setLikes(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="dislikes">
           <Form.Label >Dislikes</Form.Label>
-          <Form.Control size="sm" type="text" as="textarea" />
+          <Form.Control size="sm" type="text" as="textarea" onChange={e => setDislikes(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="img">
           <Form.Label>Image</Form.Label>
-          <Form.Control type="file" />
+          <Form.Control type="file" onChange={e => setImage(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="background colour">
           <Form.Label htmlFor="exampleColorInput">Choose your Background color</Form.Label>
-          <Form.Control type="color" id="exampleColorInput" defaultValue="#FFFFFF" placeholder="Choose your color" required />
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#FFFFFF" placeholder="Choose your color" onChange={e => setBackground(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="name font colour">
           <Form.Label htmlFor="exampleColorInput">Choose your Name font color</Form.Label>
-          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" required />
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" onChange={e => setNamecol(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="job font colour">
           <Form.Label htmlFor="exampleColorInput">Choose your Job font color</Form.Label>
-          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" required />
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" onChange={e => setJobcol(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="age font colour">
           <Form.Label htmlFor="exampleColorInput">Choose your Age font color</Form.Label>
-          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" required />
+          <Form.Control type="color" id="exampleColorInput" defaultValue="#000000" placeholder="Choose your color" onChange={e => setAgecol(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="about">
           <Form.Label >Tell us about yourself</Form.Label>
-          <Form.Control size="lg" type="text" as="textarea" required onChange={e => setJobTitle(e.target.value)} />
+          <Form.Control size="lg" type="text" as="textarea" required onChange={e => setAbout(e.target.value)} />
         </Form.Group>
         <Button variant="primary" type="submit">
           Save
